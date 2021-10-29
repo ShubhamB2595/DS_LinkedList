c44f476592178ae77f9a34e58526c0b7261311dc
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-29 23:14:49 +0530

    UC4 - Inserting the data is added

--- src/com/linkedlist/LinkedListMain.java
+++ src/com/linkedlist/LinkedListMain.java
@@ -10,11 +10,15 @@ public class LinkedListMain {
 		System.out.println("Welcome to custom LinkedList program");
 		
 		CustomLinkedList mylist = new CustomLinkedList();
-		mylist.add(56);
-		mylist.add(30);
+		
 		mylist.add(70);
-		mylist.print(mylist.head);
-	
+		mylist.add(56);
+		mylist.print();
+		
+		//inserting 30 at position 1 as 70, 56
+		System.out.println("\nAfter adding the 30");
+		mylist.insert(1, 30);
+		mylist.print();
 	}
 
 }
