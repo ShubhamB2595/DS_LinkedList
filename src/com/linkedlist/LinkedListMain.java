a8b019910d7c6087bfd6ced36a31c6d3c1b5d407
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-29 18:40:32 +0530

    UC1 - Creation of simple linked list

--- src/com/linkedlist/LinkedListMain.java
+++ src/com/linkedlist/LinkedListMain.java
@@ -1,10 +1,22 @@
 package com.linkedlist;
 
+import java.util.LinkedList;
+
+/*
+ * Data Structure Linked List
+ */
 public class LinkedListMain {
 
 	public static void main(String[] args) {
 	
 		System.out.println("Welcome to custom LinkedList program");
+		
+		LinkedList<Integer> mylist = new LinkedList<Integer>();
+		mylist.add(0, 56);
+		mylist.add(1, 30);
+		mylist.add(2, 70);
+	
+		System.out.println(mylist);
 	
 	}
 
