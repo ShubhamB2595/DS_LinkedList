db485c6444aee8236ded034f7441faa25034e1ad
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-29 19:22:40 +0530

    UC3 - creating linkedlist by appending data

--- src/com/linkedlist/LinkedListMain.java
+++ src/com/linkedlist/LinkedListMain.java
@@ -10,9 +10,9 @@ public class LinkedListMain {
 		System.out.println("Welcome to custom LinkedList program");
 		
 		CustomLinkedList mylist = new CustomLinkedList();
-		mylist.add(70);
-		mylist.add(30);
 		mylist.add(56);
+		mylist.add(30);
+		mylist.add(70);
 		mylist.print(mylist.head);
 	
 	}
